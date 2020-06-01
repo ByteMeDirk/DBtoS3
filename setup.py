62705cd45c3b1278e1ef101a48d4ef4ca3fa8590
Dirk Steynberg <dirkscgm@gmail.com>
2020-06-01 12:20:44 +0200

    preparing for another release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
 
 setuptools.setup(
     name='dbtos3',
-    version='0.0.2',
+    version='0.0.3',
     description='Replication & Full Load Application for multiple databases to s3',
     long_description=long_description,
     long_description_content_type="text/markdown",
